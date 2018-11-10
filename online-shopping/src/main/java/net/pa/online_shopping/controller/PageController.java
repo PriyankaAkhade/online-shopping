e708a02346464335ae0796d35a77bd68ac642ae5
Priyanka Akhade <priyankaakhade16@gmail.com>
2018-11-11 01:56:16 +0530

    successfully loaded static resources.

--- online-shopping/src/main/java/net/pa/online_shopping/controller/PageController.java
+++ online-shopping/src/main/java/net/pa/online_shopping/controller/PageController.java
@@ -2,6 +2,7 @@ package net.pa.online_shopping.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -10,7 +11,18 @@ public class PageController {
 	@RequestMapping(value = { "/", "/home", "/index" })
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting","Welcome to Spring web MVC");
+		mv.addObject("greeting", "Welcome to Spring web MVC");
+		return mv;
+	}
+
+	@RequestMapping(value = "/test")
+	public ModelAndView test(@RequestParam(value = "greeting", required = false) String greeting) {
+
+		if (greeting == null) {
+			greeting = "Hello there!";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
 		return mv;
 	}
 }
